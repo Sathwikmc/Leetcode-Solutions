ae60518783b212d3602226aa001f4ea7a48d1b91
Sathwik M C <sathwikmc9845@gmail.com>
2026-05-31 12:06:13 +0530

    Remove unnecessary shutdown hook
    Removed shutdown hook for runtime display logging.

--- Java/27. Remove Element.java
+++ Java/27. Remove Element.java
@@ -15,12 +15,5 @@ class Solution {
         }
         return l.size();
         
-    }    static {
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
-                fw.write("0");
-            } catch (Exception e) {
-            }
-        }));
-    }
+    }    
 }
